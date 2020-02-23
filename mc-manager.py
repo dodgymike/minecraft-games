7658f63b8d7f0ad1ed38e49a78f5d7116422e574
Mike Davis <mike.davis@scriptabl.com>
2020-02-23 15:10:27 +0200

    Read responses from server, which means: player count check (and restart if none) + better wait for server start + other potentials

--- mc-manager.py
+++ mc-manager.py
@@ -2,6 +2,7 @@
 
 import json
 import os,sys,time
+import select
 
 stdin_slave, stdout_master = os.pipe()
 stdin_master,stdout_slave  = os.pipe()
@@ -14,18 +15,80 @@ def load_config():
     with open('config.json', 'r') as infile:
         return json.load(infile)
 
+def send_command(command_text):
+    os.write(stdout_master, "{}\n".format(command_text))
+    print("COMMAND: {}".format(command_text))
+    time.sleep(0.5)
+
+def chars_to_read():
+  poll = select.poll()
+  poll.register(stdin_master, select.POLLIN | select.POLLPRI)
+  fd = poll.poll(0.5)
+  if len(fd):
+    f = fd[0]
+    if f[1] > 0:
+      return True
+
+  return False
+
+def print_all():
+    while chars_to_read():
+      print read_response()
+
+def read_all():
+    responses = []
+    while chars_to_read():
+      responses.append(read_response())
+ 
+    return responses
+
+def read_response():
+    if not chars_to_read():
+      return None
+
+    response = ""
+
+    while True:
+      response_char = os.read(stdin_master, 1)
+      if response_char == '\n':
+        return response
+         
+      response += response_char
+
+def has_players():
+    print_all()
+
+    send_command("/list")
+
+    list_responses = read_all()
+
+    for response in list_responses:
+        print("Player list response ({})".format(response))
+        if "There are 0 of a max " in response:
+            return False
+
+    return True
+
 pid = os.fork()
 if pid == 0:
 
     os.dup2(stdin_slave, 0)
-#    os.dup2(stdout_slave, 1)
+    os.dup2(stdout_slave, 1)
     cmd = ["./run_mc.sh","./run_mc.sh"]
     os.execv(cmd[0],cmd)
 
 else:
-    time.sleep(30)
-    #while True:
-    #    print os.read(stdin_master, 1)
+    print("Waiting for server to finish startup")
+    while True:
+      response = read_response()
+      if response == None:
+        time.sleep(1)
+        continue
+
+      print("Got response ({})".format(response))
+      if "[Server thread/INFO]: Done" in response:
+        print("Server startup complete")
+        break
 
     config = load_config()
 
@@ -80,55 +143,66 @@ else:
       "interval": interval,
     })
 
+    while not has_players():
+        print("No Players")
+        time.sleep(10)
+
     # always set world spawn point
-    os.write(stdout_master, "/difficulty peaceful\n")
-    time.sleep(0.5)
-    os.write(stdout_master, "/setworldspawn {} {} {}\n".format(spawn_x, spawn_height ,spawn_y))
-    time.sleep(0.5)
-    os.write(stdout_master, "/worldborder center 0 0\n")
-    time.sleep(0.5)
-    os.write(stdout_master, "/worldborder set 50\n")
-    time.sleep(0.5)
-    os.write(stdout_master, "/op surfmike\n")
-    time.sleep(0.5)
+    send_command("/gamemode survival @a")
+    send_command("/difficulty peaceful")
+    send_command("/setworldspawn {} {} {}".format(spawn_x, spawn_height ,spawn_y))
+    send_command("/worldborder center 0 0")
+    send_command("/worldborder set 50")
+    send_command("/op surfmike")
+
+    print_all()
 
     #for height in range(1, spawn_height):
-    #  os.write(stdout_master, "/fill -25 {} -25 25 {} 25 cobblestone\n".format(height, height+1))
+    #  send_command("/fill -25 {} -25 25 {} 25 cobblestone".format(height, height+1))
     #  time.sleep(1)
     #for height in range(spawn_height, spawn_height+5):
-    #  os.write(stdout_master, "/fill -25 {} -25 25 {} 25 air\n".format(height, height+1))
+    #  send_command("/fill -25 {} -25 25 {} 25 air".format(height, height+1))
     #  time.sleep(1)
-    #os.write(stdout_master, "/fill -25 {} -25 25 {} 25 oak_planks\n".format(spawn_height, spawn_height-5))
+    #send_command("/fill -25 {} -25 25 {} 25 oak_planks".format(spawn_height, spawn_height-5))
     #time.sleep(1)
 
     while True:
-      os.write(stdout_master, "/worldborder center {} {}\n".format(spawn_x, spawn_y))
+      if not has_players():
+          print("No Players")
+          time.sleep(10)
+          continue
+
+      send_command("/worldborder center {} {}".format(spawn_x, spawn_y))
+      print_all()
 
       while True:
-        os.write(stdout_master, "/say Remember kids, lava is the floor\n")
+        send_command("/say Remember kids, lava is the floor")
+        print_all()
 
         time_left = 30
         while time_left > 0:
-            os.write(stdout_master, "/say {} seconds left until lava goes up\n".format(time_left))
+            send_command("/say {} seconds left until lava goes up".format(time_left))
             time_left -= interval
             time.sleep(interval)
 
-        os.write(stdout_master, "/fill {} {} {} {} {} {} lava keep\n".format(spawn_x-25, lava_level-lava_depth, spawn_y-25, spawn_x + 25, lava_level, spawn_y + 25))
+        send_command("/fill {} {} {} {} {} {} lava keep".format(spawn_x-25, lava_level-lava_depth, spawn_y-25, spawn_x + 25, lava_level, spawn_y + 25))
         time.sleep(0.5)
-        os.write(stdout_master, "/fill {} {} {} {} {} {} glass hollow\n".format(spawn_x-5, lava_level+5, spawn_y-5, spawn_x+5, lava_level+15, spawn_y+5))
+        send_command("/fill {} {} {} {} {} {} glass hollow".format(spawn_x-5, lava_level+5, spawn_y-5, spawn_x+5, lava_level+15, spawn_y+5))
         time.sleep(0.5)
-        os.write(stdout_master, "/fill {} {} {} {} {} {} cobblestone\n".format(spawn_x+3, lava_level+5, spawn_y+3, spawn_x+1, lava_level+6, spawn_y+1))
+        send_command("/fill {} {} {} {} {} {} cobblestone".format(spawn_x+3, lava_level+5, spawn_y+3, spawn_x+1, lava_level+6, spawn_y+1))
         time.sleep(0.5)
-        os.write(stdout_master, "/fill {} {} {} {} {} {} oak_planks\n".format(spawn_x-25, 1, spawn_y-25, spawn_x-25, 255, spawn_y-25))
+        send_command("/fill {} {} {} {} {} {} oak_planks".format(spawn_x-25, 1, spawn_y-25, spawn_x-25, 255, spawn_y-25))
         time.sleep(0.5)
-        os.write(stdout_master, "/fill {} {} {} {} {} {} oak_planks\n".format(spawn_x+4, lava_level+5, spawn_y+4, spawn_x+5, lava_level+6, spawn_y+5))
+        send_command("/fill {} {} {} {} {} {} oak_planks".format(spawn_x+4, lava_level+5, spawn_y+4, spawn_x+5, lava_level+6, spawn_y+5))
         time.sleep(0.5)
-        os.write(stdout_master, "/fill {} {} {} {} {} {} iron_ore\n".format(spawn_x-4, lava_level+5, spawn_y-4, spawn_x-5, lava_level+6, spawn_y-5))
+        send_command("/fill {} {} {} {} {} {} iron_ore".format(spawn_x-4, lava_level+5, spawn_y-4, spawn_x-5, lava_level+6, spawn_y-5))
         time.sleep(0.5)
-        os.write(stdout_master, "/fill {} {} {} {} {} {} coal_ore\n".format(spawn_x-4, lava_level+5, spawn_y+4, spawn_x-5, lava_level+6, spawn_y+5))
+        send_command("/fill {} {} {} {} {} {} coal_ore".format(spawn_x-4, lava_level+5, spawn_y+4, spawn_x-5, lava_level+6, spawn_y+5))
         time.sleep(0.5)
-        os.write(stdout_master, "/setworldspawn {} {} {}\n".format(spawn_x, lava_level+6, spawn_y))
+        send_command("/setworldspawn {} {} {}".format(spawn_x, lava_level+6, spawn_y))
         time.sleep(0.5)
+        print_all()
+
         lava_level += lava_depth
         config["lava_level"] = lava_level
         save_config({
@@ -141,8 +215,12 @@ else:
         })
 
         if lava_level >= 255:
-          os.write(stdout_master, "/say GAME OVER\n")
+          send_command("/say GAME OVER")
           break
+
+        if not has_players():
+            print("No Players")
+            break
 
       spawn_x += 150
       if spawn_x > 1000:
